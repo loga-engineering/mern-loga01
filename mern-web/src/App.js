c398682af2eb76785ae6796a18772b901835406b
Sidaty Moulaye KOUREICHY <ksidatym@gmail.com>
2021-11-29 19:28:10 +0000

    UseState, UseEffect

---
+++ mern-web/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Person from "./Person";
+import Address from "./Address";
+
+const App = () => {
+    return (
+        <div>
+            <Person/>
+            {/*<Address/>*/}
+        </div>
+    )
+}
+
+export default App;
